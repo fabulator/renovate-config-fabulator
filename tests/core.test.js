ba1ff5a0c80484f0f158a9949f02d364bb2a4536
Michal Ozogán <michal@ozogan.eu>
2020-05-23 03:34:04 +0200

    Changed: Update packages; Fixed: Linter; Fixed: Updating patch releases

---
+++ tests/core.test.js
@@ -0,0 +1,12 @@
+const { validateConfig } = require('renovate/dist/config/validation');
+const pkg = require('../package.json');
+
+const configs = pkg['renovate-config'];
+
+test('Renovate config should be valid', () => {
+    Object.keys(configs).forEach(async (presetName) => {
+        const { errors, warnings } = await validateConfig(configs[presetName]);
+        expect(errors).toHaveLength(0);
+        expect(warnings).toHaveLength(0);
+    });
+});
